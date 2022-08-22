f49f0a419520a84fa58b104ad2782d0705a70b06
clandestine33 <ed.oguamanam@gmail.com>
2022-08-22 15:20:29 +0100

    bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController')
 
 router.post('/', authController.handleLogin);
 router.post('/register', authController.handleNewUser);
-router.get('/user', authController.getUser)
+router.get('/user', userController.getUser)
 router.post('/user')
 
 module.exports = router;
